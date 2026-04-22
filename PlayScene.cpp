119d7557d2f3a31391fad56bfaae760ab851f523
250579 <250579@jc-21.jp>
2026-04-22 12:16:18 +0900

    時とめたい。 次の俺に任せよう。

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -1,6 +1,6 @@
 #include "PlayScene.h"
 #include"Engine/Model.h"
-
+#include"Engine/Input.h"
 //コンストラクタ
 //引数：parent  親オブジェクト（SceneManager）
 //メンバー変数の初期化はコンストラクタで
@@ -16,17 +16,31 @@ void PlayScene::Initialize()
 
 void PlayScene::Update()
 {
-	static float count = 0.0f;
-	static float dir = 1.0;
-	static float time = 0.0f;
-	time += 0.1f;
-	count+=0.1f * dir;
-	ot_.rotate_ = { ot_.rotate_.x+1.0f,ot_.rotate_.y + 1.0f,ot_.rotate_.z + 1.0f };
-	ot_.scale_ = { 1.0f,1.0f,2.0f };
-	ot_.position_ = { count,ot_.position_.y+( - 0.5f) * sinf(time),10.0f};
-	if(count>10|| count < -10)
-		dir *= -1;
-	
+	static float pauseTime = 2.0f;
+	static bool isPause = false;
+	if (Input::IsKeyDown(DIK_SPACE)) {
+		pauseTime -= 0.1f;
+		isPause = true;
+		if (pauseTime < 0.0f) {
+			isPause = false;
+			pauseTime = 2.0f;
+		}
+	}
+	if(!isPause){
+		static float count = 0.0f;
+		static float dir = 1.0;
+		static float time = 0.0f;
+		time += 0.1f;
+		count += 0.1f * dir;
+		ot_.rotate_ = { ot_.rotate_.x + 1.0f,ot_.rotate_.y + 1.0f,ot_.rotate_.z + 1.0f };
+		ot_.scale_ = { 1.0f,1.0f,2.0f };
+		ot_.position_ = { count,ot_.position_.y + (-0.5f) * sinf(time),10.0f };
+		if (count > 10 || count < -10)
+			dir *= -1;
+	}
+	else {
+
+	}
 }
 
 void PlayScene::Draw()
