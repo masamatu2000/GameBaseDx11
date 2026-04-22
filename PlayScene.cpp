5990bc1024483de548db1ede0da78a914cd47160
250579 <250579@E0404-05>
2026-04-22 12:07:51 +0900

    プロジェクトファイルを追加。

---
+++ PlayScene.cpp
@@ -0,0 +1,43 @@
+#include "PlayScene.h"
+#include"Engine/Model.h"
+
+//コンストラクタ
+//引数：parent  親オブジェクト（SceneManager）
+//メンバー変数の初期化はコンストラクタで
+PlayScene::PlayScene(GameObject* parent) :GameObject(parent, "PlayScene"), hModel_(-1)
+{
+}
+
+void PlayScene::Initialize()
+{
+	hModel_ = Model::Load("../Assets/Oden.fbx");
+	assert(hModel_ >=0);
+}
+
+void PlayScene::Update()
+{
+	static float count = 0.0f;
+	static float dir = 1.0;
+	static float time = 0.0f;
+	time += 0.1f;
+	count+=0.1f * dir;
+	ot_.rotate_ = { ot_.rotate_.x+1.0f,ot_.rotate_.y + 1.0f,ot_.rotate_.z + 1.0f };
+	ot_.scale_ = { 1.0f,1.0f,2.0f };
+	ot_.position_ = { count,ot_.position_.y+( - 0.5f) * sinf(time),10.0f};
+	if(count>10|| count < -10)
+		dir *= -1;
+	
+}
+
+void PlayScene::Draw()
+{
+	
+	if (hModel_ != -1) {
+		Model::SetTransform(hModel_,ot_);
+		Model::Draw(hModel_);
+	}
+}
+
+void PlayScene::Release()
+{
+}
---
+++ TestScene.cpp
@@ -0,0 +1,41 @@
+#include "TestScene.h"
+#include"Engine/Input.h"
+#include"Engine/SceneManager.h"
+#include"Engine/Image.h"
+//コンストラクタ
+TestScene::TestScene(GameObject * parent)
+	: GameObject(parent, "TestScene"),hTitlePic_(-1)
+{
+}
+
+//初期化
+void TestScene::Initialize()
+{
+	hTitlePic_ = Image::Load("../Assets/Title.png");
+	assert(hTitlePic_ >-1);
+}
+
+//更新
+void TestScene::Update()
+{
+	if (Input::IsKeyDown(DIK_SPACE)) {
+		//SceneManager* pSceneManager = (SceneManager*)FindObject("SceneManager");
+		SceneManager* pSceneManager = (SceneManager*)(this->GetParent());
+		pSceneManager->ChangeScene(SCENE_ID_PLAY);
+	}
+}
+
+//描画
+void TestScene::Draw()
+{
+	if (hTitlePic_ != -1) {
+		transform_.scale_ = { 1.5f,1.5f,1.0f };
+		Image::SetTransform(hTitlePic_,transform_);
+		Image::Draw(hTitlePic_);
+	}
+}
+
+//開放
+void TestScene::Release()
+{
+}
